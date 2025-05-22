5ba12d56992c43da4580b11349ec50950bad1f51
Nandini Giri <118757788+NNDINI7@users.noreply.github.com>
2025-05-22 20:10:23 +0530

    Add files via upload

---
+++ wpmessenger.py
@@ -0,0 +1,5 @@
+import pywhatkit
+# This script sends a WhatsApp message to a specified number at a specified time.
+# Make sure to replace the phone number with your own and adjust the time as needed.
+pywhatkit.sendwhatmsg("+918432447259", "Hello, this is a test message!", 3, 0)
+
